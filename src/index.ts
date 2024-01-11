50979dbb0dc7025f2d14118d459ff160888a1db8
Jose Teran <josedavidteran25@@gmail.com>
2024-01-10 23:55:07 -0500

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import accionesRoutes from './routes/acciones-routers';
 
 import * as dotenv from 'dotenv';
@@ -7,7 +8,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
+// Habilitar CORS para todas las rutas
+app.use(cors());
 app.use(express.json());
 app.use(accionesRoutes);
 app.listen(PORT);
