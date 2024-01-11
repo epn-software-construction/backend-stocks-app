42b27de573e7f5adc6da0541da92f41e7f8ad0b9
Alex Padilla <0xpadimaster@gmail.com>
2024-01-10 22:50:49 -0500

    fix jest and docker implementation

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  clearMocks: true,
-  coverageProvider: 'v8',
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+    clearMocks: true,
+    coverageProvider: 'v8',
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import * as express from 'express';
 import accionesRoutes from './routes/acciones-routers';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 4000;
+
 app.use(express.json());
 app.use(accionesRoutes);
-app.listen(4000);
+app.listen(PORT);
 
-console.log('Server on port', 4000);
+console.log(`Server running on port ${PORT}`);
